d60bef3b49be4d3d84fc806f3ab3c86a9a000b61
DivyaAhire11 <divyaahire121@gmail.com>
2025-08-24 17:06:27 +0530

    Datatypes in php

---
+++ Concept/DataTy.php
@@ -0,0 +1,44 @@
+<?php 
+/*Scalar Type
+   1.Integer
+   2.Float
+   3.String
+   4.Boolean
+
+   Compound Type
+   5.Array
+   6.Object
+
+   Special Type
+   7.NULL
+   8.Resource
+
+*/
+
+$age = 25;
+$balance = -1000;
+$big = 7392739749374389475903275;
+$greet = 'hello';
+
+// echo var_dump($age); //int type
+// echo var_dump($balance);  //int type
+// echo var_dump($big);  //float type
+// echo var_dump($greet);  //string type
+
+$city = "Mumbai";
+// echo "I Live in $city\n";  
+// I Live in Mumbai
+// echo 'I live in $city'; 
+// I live in $city
+
+$a=false;
+echo $a; //not printing
+
+$fruit = ["mango","banana","apple"];
+echo $fruit[0],"\n";
+echo $fruit[1],"\n";
+echo $fruit[2],"\n";
+
+
+
+?>
---
+++ Concept/info.php
@@ -0,0 +1,17 @@
+<?php
+// identifiers are the name you give to variables,function,classes,constants etc
+//identifier = Name we assign to variables,functions,classes,constant etc
+//$Student is an identifier for a variable 
+$Student = "divya";
+
+//constant
+
+define ("PI",3.14);
+echo PI; //3.14
+$PI = 999;
+echo "\n",$PI;   //999
+echo "\n",PI;    //3.14
+
+//keyword = reserved word 
+
+?>
