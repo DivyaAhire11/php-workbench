66ac544815e26ffd6d8bbecf8088889ef5ab3570
DivyaAhire11 <divyaahire121@gmail.com>
2025-08-24 17:24:52 +0530

    more details about datatypes

--- Concept/DataTy.php
+++ Concept/DataTy.php
@@ -26,19 +26,36 @@ $greet = 'hello';
 // echo var_dump($greet);  //string type
 
 $city = "Mumbai";
-// echo "I Live in $city\n";  
+echo "I Live in $city <br>";  
 // I Live in Mumbai
-// echo 'I live in $city'; 
+echo 'I live in $city <br>'; 
 // I live in $city
 
 $a=false;
-echo $a; //not printing
+echo $a; 
 
 $fruit = ["mango","banana","apple"];
-echo $fruit[0],"\n";
-echo $fruit[1],"\n";
-echo $fruit[2],"\n";
+echo $fruit[0],"<br>";
+echo $fruit[1],"<br>";
+echo $fruit[2],"<br>";
+
+$student = ["name"=> "divya","age"=> 20];
+echo $student["name"]; //divya
 
 
+class Car{
+    public $name;
+
+    function setName($name) {
+        $this->name = $name; 
+    }
+}
+
+$myCar = new Car();
+$myCar->setName("BMW");  
+echo $myCar->name;       
+
+$x = NULL;
+echo $x;
 
 ?>
