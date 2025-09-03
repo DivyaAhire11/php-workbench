e2a82093e178f5815e78165ce88a38f85d84391c
DivyaAhire11 <divyaahire121@gmail.com>
2025-09-03 22:04:22 +0530

    practice more str functions

---
+++ Strfunctions/StrFun3.php
@@ -0,0 +1,15 @@
+<?php
+   //ucwords($str) : First letter of each word uppercase
+
+   echo ucfirst("my name is divya."),"\n";   //My name is divya.
+   echo ucwords("php is fun"),"\n";   //Php Is Fun
+   echo ucwords("my name is divya."),"\n";   //My Name Is Divya.
+
+   $m = "    Hello    ";
+   echo trim($m); //Remove whitespace
+   echo ltrim($m);
+   echo rtrim($m);
+
+
+   echo str_repeat(" Ritu ",10); 
+?>
