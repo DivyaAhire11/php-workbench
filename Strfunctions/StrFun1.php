17954324e69876f82d5c45862888e09a6626b3c4
DivyaAhire11 <divyaahire121@gmail.com>
2025-09-03 21:22:18 +0530

    modification in strfunction

--- Strfunctions/StrFun1.php
+++ Strfunctions/StrFun1.php
@@ -1,15 +1,26 @@
 <?php
    //length and count function
+   //strlen($str) : return lenght of string
+   //str_word_count($str) : return word count
+   //substr_count($str, substring) : Count the number of substring occurrences
+
    $name = "Hello World"; // 11
    $len= strlen($name);
 
    echo "Lenght of $name is : $len"; //Lenght of Hello World is : 11
 
-   echo "\n";
+   echo "<br>";
    $msg = "PHP is a Backend language";
    $cnt = str_word_count($msg);
    echo "Word count is : $cnt"; //Word count is : 5
 
-   echo "\n";
-   echo substr_count("banana","na");
+   echo "<br>";
+   echo substr_count("banana","na");//2 =>"na" appers at : "ba[na]na"  "bana[na]" 2 times
+    echo "<br>";
+   echo substr_count($name,"o");//2 ==>"o" appears at: "Hell[o] W[o]rld" 2times
+    echo "<br>";
+   echo substr_count("banana","a");//3 ==> "a" appears at : b[a]n[a]n[a] 3times
+    echo "<br>";
+   echo substr_count($msg,"Backend");//1 ==> appears at 1 time 
+    echo "<br>";
 ?>
