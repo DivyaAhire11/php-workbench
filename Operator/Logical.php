e15db003724c15d36baccc92f158df2a0b969653
DivyaAhire11 <divyaahire121@gmail.com>
2025-09-06 18:10:01 +0530

    more practice

--- Operator/Logical.php
+++ Operator/Logical.php
@@ -1,6 +1,6 @@
 <?php
 
-//Logical Operators
+// Logical Operators
 
 $a = true;
 $b = false;
@@ -8,32 +8,46 @@ $b = false;
 echo "true && false : \t";
 var_dump(true && false);   // false
 echo "<br><br>";
+
 echo "true || false : ";
 var_dump(true || false);   // true
 echo "<br><br>";
+
 echo "!true : ";
 var_dump(!true);        // false
 echo "<br><br>";
+
 echo "true and false : ";
 var_dump(true and false);  // false
 echo "<br><br>";
+
 echo "true or false : ";
 var_dump(true or false);   // true
 echo "<br><br>";
+
 echo "true xor false : ";
 var_dump(true xor false);  // true
 echo "<br><br>";
+
 echo "true xor true : ";
 var_dump(true xor true);  // false (both same)
 echo "<br><br>";
+
 echo "(5 > 3) && (2 < 4) : ";
 var_dump((5 > 3) && (2 < 4));  // true
 echo "<br><br>";
+
 echo "(5 > 10) || (10 > 2) : ";
 var_dump((5 > 10) || (10 > 2)); // true
 echo "<br><br>";
+
 echo "!(5 == 5) : ";
 var_dump(!(5 == 5));  // false
 echo "<br><br>";
+
+$x =22;
+$y =6;
+echo "$x <> $y = ";
+echo var_dump($x <> $y); //true
 
 ?>
--- demo.php
+++ demo.php
@@ -1,14 +1,30 @@
 <?php
-$a = array(1=>"hii ",2=>"php ");
-// echo $a;
-echo $a[1];
-echo "<br>";
-echo $a[2];
-echo "<br>";
-// echo $a[0];
+// $a = array("C","php","java","python");
+// echo var_dump($a);
 
-$n="divya";
-echo isset($n);
+// echo "<br>";
+// echo $a[1];
+// echo "<br>";
+
+// echo $a[2];
+// echo "<br>";
+
+// echo $a[3];
+// echo "<br>";
+
+// echo $a[0];
+// echo "<br>";
+
+// // echo $a[4]; //undefined
+// echo "<br>";
+
+// $n="divya";
+// echo isset($n);
+
+$name = NULL;
+echo $name; //not print
+echo var_dump($name); //NULL
+
 
 
 ?>
